4e6938bc747795d90b0357df23f7db7908deb200
Jair <dev.lnx1337@gmail.com>
2013-12-17 14:33:16 -0600

    Se corrigio la indentación de controlador  y modelo User.js, cambios logica del modelo, cambios en los comentarios de los commits
    *Se corrigio la indentación de los archivos /app/models/user.js y el
    archivo /app/routes/user.js
    *Se corrigio la logica en el modelo
    *Se corrigieron buenas practicas en comparadores logicos ===
    *Se pretenden mejorar los comentarios en los commits

--- app/models/user.js
+++ app/models/user.js
@@ -1,138 +1,113 @@
 var Db = require('mysql-activerecord');
 var db = new Db.Adapter({
-    server: 'localhost',
-    username: 'root',
-    password: 'D3c3p710n',
-    database: 'intangibledb'
+  server: 'localhost',
+  username: 'root',
+  password: '',
+  database: 'intangibledb'
 });
 
-
 exports.truncate=function(callback){
-
-db.query('truncate table tbl_users', function(err, results) {  
-        if(!err){
-        	 callback({response:"Database truncated!"});
-        }
-	});
+  
+  db.query('truncate table tbl_users', function(err, results) {  
+    var response={};
+    response.data=results;
+    response.error=err;
+    callback(response);
+  });
 
 }
-
 
 exports.validateLogin=function(model,callback){
-   db.select('id,email,password').where('email= "'+ model.email +'" and password="'+model.password+'"').get('tbl_users',function(err,data){     
-                
-              if(!err){
-                 if(data!=""){
-                 	 callback(data);
-                 }else{
-                 	callback("");
-                 }
-             }else{
-              	callback({error:err});
-              }
-            
-   });
-}
 
+  db.select('id,email,password').where('email= "'+model.email+'" and pasword="'+
+  model.password+'"').get('tbl_users',function(err,data){     
+    var response={};
+    response.data=data;
+    response.error=err;   
+    callback(response); 
+  });
+
+}
 
 exports.findAll=function(callback){
-	db.select('nombre,apellido,email,password').get('tbl_users',function(err,data){
-         if(!err){
-                 if(data.length>0){
-                 	 callback(data);
-                 }else{
-                 	callback("");
-                 }
-          }else{
-              	callback({error:err});
-          }
-	});
-}
 
+  db.select('nombre,apellido,email,password').get('tbl_users',function(err,data){
+    var response={};
+    response.data=data;
+    response.error=err;
+    callback(response);
+  });
+
+}
 
 exports.save=function(model,callback){
 
+  db.select('id,nombre,apellido,email,password').where('email="'+model.email+'" and password="'+model.password+'"').get('tbl_users',function(err,data){
+    if(data!=""){
+      callback({data:"",error:"The username already exists"});  
+    }else{
+      db.insert('tbl_users', { nombre: model.nombre , apellido: model.apellido ,email:model.email ,password:model.password }, function(err, data){                 
+        var response={}; 
+        response.data=data;
+        response.error=err;
+        callback(response);
+      });
+    }  
+  });    
 
-    db.select('id,nombre,apellido,email,password').where('email="'+model.email+'" and password="'+model.password+'"').get('tbl_users',function(err,data){
-      
-      if(data!=""){
-            callback({response:"The username already exists"});
-      
-      }else{
-	 	db.insert('tbl_users', { nombre: model.nombre , apellido: model.apellido ,email:model.email ,password:model.password }, function(err, data){                 
-			            if(!err){
-			                 if(data.insertId!=null){
-			            	     console.log(data);
-			                 	 callback(data);
-			                 }else{
-			                 	callback("");
-			                 }
-			          }else{
-			              	callback({error:err});
-			          }
-			});
-
-      }  
-
-      
-    
-    });    
 }
 
-
 exports.findById=function(model,callback){
+
   db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,data){
-          if(!err){
-                 if(data.length>0){
-                 	 callback(data);
-                 }else{
-                 	callback("");
-                 }
-          }else{
-              	callback({error:err});
-          }
-           
+    var response={};
+    if(data.length>0){
+      response.data=data;
+      response.error=err;
+    }else{
+     response.data={response:"User not found"};
+     response.error=err; 
+    }
+    callback(response);    
   });
+
 }
 
 exports.update=function(model,callback){
-
- db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,data){
-            if(data.length>0){
-                   db.where({id:model.id});
-                   db.update('tbl_users', model.body , function(err){
-                   if (!err) {
-                         db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,result){
-                             if(result.length>0){
-                             	 result[0].update="ok";
-                             	 callback(result);
-                             }
-                         });
-
-                     }
-                  });
-            }else{
-            	callback("");
-            }   
+ 
+  db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,data){
+    if(data.length>0){
+      db.where({id:model.id});
+      db.update('tbl_users', model.body , function(err){
+        if(!err) {
+          db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,result){
+            var response={};
+            result[0].update="ok";
+            response.data=result;
+            response.error=err;
+            callback(response);
+          });
+        }
       });
-}
+    }else{
+      callback({data:"",error:"User not found"});
+    }   
+  });
 
+}
 
 exports.delete=function(model,callback){
 
- db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,data){
-            if(data.length>0){
-					    db.where({ id: model.id }).delete('tbl_users', function(err) {
-					        if (!err) {
-					            callback({response:"delete"});
-					        }else{
-					           callback("");
-					        }
-					    });
-	         }else{
-                 callback({response:""});
-            }   
-        });	
-
+  db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,data){
+    if(data.length>0){
+      db.where({ id: model.id }).delete('tbl_users', function(err) {
+        var response={};
+        response.data={response:"Deleted"};
+        response.error=err;  
+      });
+    }else{
+      callback({data:"",error:"User not found"});
+    }   
+  }); 
 
 }
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,25 +1,22 @@
 /*
  API RESTFUL USERS
  */
- var model=require('../models/user.js');
-
-
-
+var model=require('../models/user.js');
 var Db = require('mysql-activerecord');
 var db = new Db.Adapter({
-    server: 'localhost',
-    username: 'root',
-    password: 'D3c3p710n',
-    database: 'intangibledb'
+  server: 'localhost',
+  username: 'root',
+  password: '',
+  database: 'intangibledb'
 });
 
 
 
 /* Truncar tabla */
 exports.db=function(req,res){
-	model.truncate(function(callback){
-          res.send(callback);
-	})
+  model.truncate(function(callback){
+    res.send(callback);
+  })
 }
 
 /*
@@ -28,47 +25,52 @@ exports.db=function(req,res){
    params: username,password
 */
 exports.login=function(req,res){
-   var email=req.body.email;
-   var password=req.body.password;
-   if(email.length !=0 && password.length !=0){
-
-      if(validateEmail(email)){
-	         
-	         model.validateLogin({email:email,password:password},function(callback){   
-		             if(callback.length>0){
-		                   res.status(200).send(callback);
-		             }else{
-		            	   res.status(200).send({response:"User and password are incorrect "});
-		             } 
-	          });
-         
-         }else{
-         
-         	res.status(200).send({response:"Mail is not valid: example@example.com"})
-         
-         }
-  
-
+ 
+  var email=req.body.email.trim();
+  var password=req.body.password.trim();
+ 
+  if(email!=null && email!="" && password!=null && password!=""){
+    if(validateEmail(email)){
+      model.validateLogin({email:email,password:password},function(callback){
+        if(callback.error==="" || callback.error==null){ 
+          if(callback.data.length>0){
+            res.status(200).send(callback.data);
+          }else{
+            res.status(200).send({response:"User and password are incorrect "});
+          } 
+        }else{
+          res.status(200).send(callback.error);
+        }
+      });    
     }else{
-  	          res.status(200).send({response:"User and password are required "});
+      res.status(200).send({response:"Mail is not valid: example@example.com"})
     }
+
+  }else{
+    res.status(200).send({response:"User and password are required "});
+  }
+
 }
+
 
 /*
    method: list 
    request GET 
    params: none
 */
-
 exports.list = function(req, res){
-    model.findAll(function(callback){
-            if(callback!=""){
-                 res.status(200).send(callback);
-            }else{
-            	 res.status(200).send({response:"No users"});
-            }   
-     });
+  
+  model.findAll(function(callback){
+    if(callback.error==="" || callback.error==null){
+      res.status(200).send(callback.data);
+    }else{
+      res.status(200).send(callback.error);
+    } 
+  });
+
 };
+
+
 
 /*
    method: create 
@@ -77,38 +79,27 @@ exports.list = function(req, res){
 */
 
 exports.create=function(req,res){
-
-      var nombre=req.body.nombre;
-      var apellido=req.body.apellido;
-      var email=req.body.email;
-      var password=req.body.password;
-
-
-      if( nombre !=" "  && apellido !=" " && email !=" " && password !=" " ){
-
-       if(validateEmail(email)){
-       
-        model.save({ nombre: nombre , apellido: apellido ,email:email ,password:password }, function(callback){  
-           
-           if(!callback.error){
-             	res.status(200).send(callback);
-            }else{
-            	res.status(200).send(callback);
-            }
-        });
-
-         }else{
-         
-         	res.status(200).send({response:"Mail is not valid: example@example.com"})
-         
-         }
-
-
-
-
-      }else{
-      	res.status(200).send({response:"Error all data is required"});
-      }
+  
+  var nombre=req.body.nombre.trim();
+  var apellido=req.body.apellido.trim();
+  var email=req.body.email.trim();
+  var password=req.body.password.trim();
+  
+  if(nombre !=null && nombre.length>0  && apellido !=null && apellido.length>0  && email !=null  && email.length>0 && password !=null && password.length>0 ){
+    if(validateEmail(email)){   
+      model.save({ nombre: nombre , apellido: apellido ,email:email ,password:password }, function(callback){  
+        if(!callback.error){
+          res.status(200).send(callback);
+        }else{
+          res.status(200).send(callback);
+        }
+      });
+    }else{
+      res.status(200).send({response:"Mail is not valid: example@example.com"})
+    }
+  }else{
+    res.status(200).send({response:"Error all data is required"});
+  }
 }
 
 /*
@@ -118,19 +109,21 @@ exports.create=function(req,res){
 */
 
 exports.view=function(req,res){
-	var id=req.params.id;
-	if(id.match(/^\d+/)){
-	     model.findById({id:id},function(callback){
-	            if(callback.length>0){
-	                 res.status(200).send(callback);
-	            }else{
-	            	res.status(200).send({response:"User not found"});
-	            }   
-	      });
+  
+  var id=req.params.id;
+  
+  if(id.match(/^\d+/)){
+    model.findById({id:id},function(callback){
+      if(callback.error===null){
+        res.status(200).send(callback.data);
+      }else{
+        res.status(200).send(callback.error);
+      }   
+    });
 
-	   }else{
-	   	  res.status(430).send({response:"Invalid Request Error! "});
-	   } 
+  }else{
+    res.status(430).send({response:"Invalid Request Error! "});
+  } 
 }
 
 /*
@@ -139,19 +132,19 @@ exports.view=function(req,res){
    params:id user id 
 */
 exports.update=function(req,res){
-
- var id=req.params.id;
- if(id.match(/^\d+/)){
-     model.update({id:id,body:req.body}, function(callback){
-                 if(callback.length>0){
-                        res.status(200).send(callback);
-                   }else{ 
-            	      res.status(200).send({response:"User not found"});
-                  }              
-     });
-   }else{
-   	  res.status(430).send({response:"Invalid Request Error! "});
-   } 
+  
+  var id=req.params.id;
+  if(id.match(/^\d+/)){
+    model.update({id:id,body:req.body}, function(callback){
+      if(callback.error===null){
+        res.status(200).send(callback.data);
+      }else{ 
+        res.status(200).send(callback);
+      }              
+    });
+  }else{
+    res.status(430).send({response:"Invalid Request Error! "});
+  } 
 }
 
 /*
@@ -160,25 +153,24 @@ exports.update=function(req,res){
    params:id user id 
 */
 exports.delete=function(req,res){
-	
-	var id=req.params.id;
-    
-    if(id.match(/^\d+/)){
-      model.delete({id:id},function(callback){
-            if(callback.response=="delete"){
-					    res.status(200).send(callback);
-	         }else{
-            	        res.status(200).send({response:"User not Found"});
-             }   
-        });	
-   }else{
-   	  res.status(430).send({response:"Invalid Request Error! "});
-   } 
+  
+  var id=req.params.id;
+  
+  if(id.match(/^\d+/)){
+    model.delete({id:id},function(callback){
+      if(callback.error===null){
+        res.status(200).send(callback);
+      }else{
+        res.status(200).send(callback.error);
+      }   
+    }); 
+  }else{
+    res.status(430).send({response:"Invalid Request Error! "});
+  } 
 
 }
 
-
 function validateEmail(email) { 
-    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/; 
+  return re.test(email);
 } 
