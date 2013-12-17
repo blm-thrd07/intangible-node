09511fe16a68ef7fe7f5904c1ec2195f12d67d4a
Jair <dev.lnx1337@gmail.com>
2013-12-17 14:44:48 -0600

    Correccion de buenas practicas de indentacion
    se corrigieron en archivos /app/models/user.js

--- app/models/user.js
+++ app/models/user.js
@@ -42,15 +42,17 @@ exports.findAll=function(callback){
 
 exports.save=function(model,callback){
 
-  db.select('id,nombre,apellido,email,password').where('email="'+model.email+'" and password="'+model.password+'"').get('tbl_users',function(err,data){
+  db.select('id,nombre,apellido,email,password').where('email="'+model.email+
+    '" and password="'+model.password+'"').get('tbl_users',function(err,data){
     if(data!=""){
       callback({data:"",error:"The username already exists"});  
     }else{
-      db.insert('tbl_users', { nombre: model.nombre , apellido: model.apellido ,email:model.email ,password:model.password }, function(err, data){                 
-        var response={}; 
-        response.data=data;
-        response.error=err;
-        callback(response);
+      db.insert('tbl_users', { nombre: model.nombre , apellido: model.apellido,
+        email:model.email ,password:model.password }, function(err, data){                 
+          var response={}; 
+          response.data=data;
+          response.error=err;
+          callback(response);
       });
     }  
   });    
@@ -59,41 +61,43 @@ exports.save=function(model,callback){
 
 exports.findById=function(model,callback){
 
-  db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,data){
-    var response={};
-    if(data.length>0){
-      response.data=data;
-      response.error=err;
-    }else{
-     response.data={response:"User not found"};
-     response.error=err; 
-    }
-    callback(response);    
-  });
+  db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',
+    function(err,data){
+      var response={};
+      if(data.length>0){
+        response.data=data;
+        response.error=err;
+      }else{
+       response.data={response:"User not found"};
+       response.error=err; 
+      }
+      callback(response);    
+    });
 
 }
 
 exports.update=function(model,callback){
  
-  db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,data){
-    if(data.length>0){
-      db.where({id:model.id});
-      db.update('tbl_users', model.body , function(err){
-        if(!err) {
-          db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,result){
-            var response={};
-            result[0].update="ok";
-            response.data=result;
-            response.error=err;
-            callback(response);
-          });
-        }
-      });
-    }else{
-      callback({data:"",error:"User not found"});
-    }   
-  });
-
+  db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',
+    function(err,data){
+      if(data.length>0){
+        db.where({id:model.id});
+        db.update('tbl_users', model.body , function(err){
+          if(!err) {
+            db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',
+              function(err,result){
+                var response={};
+                result[0].update="ok";
+                response.data=result;
+                response.error=err;
+                callback(response);
+            });
+          }    
+        });
+      }else{
+        callback({data:"",error:"User not found"});
+      }   
+    });
 }
 
 exports.delete=function(model,callback){
