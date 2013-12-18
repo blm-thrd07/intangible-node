a7503ea744e85638d41fd1ace196cc3d41eab054
Jair <dev.lnx1337@gmail.com>
2013-12-18 13:37:34 -0600

    Se añadieron comentarios a /app/model/user.js
    *Se eliminaron lineas sobrantes en /app/routes/user.js
    *Se explico cada metodo de el modelo ubicado  en /app/model/user.js
    *Se añadio validación sql injection
    *Validacion de datos de entrada

--- app/models/user.js
+++ app/models/user.js
@@ -1,13 +1,37 @@
+// This file is the model, and has features that allow us to communicate with 
+// mysql database here consultations conducted validate the input data.
+
+
+// Include activerecord-mysql module which allows us to communicate with the database Through 
+// his meincluimos activerecord-mysql module which allows us to communicate with the database through his methods
+
+// db var is our object to access our database
+// server is the database server
+// username and password of the database
+// database is the name of the database
+
 var Db = require('mysql-activerecord');
 var db = new Db.Adapter({
-  server: 'localhost',
+  server: 'localhost', 
   username: 'root',
   password: '',
   database: 'intangibledb'
 });
 
+
+// Model data
+// nombre varchar
+// apellido varchar
+// email varchar
+// password varchar
+
+
+// The function allows us to truncate the table empty tbl users
+// Returns a callback with the response.
 exports.truncate=function(callback){
-  db.query('truncate table tbl_users', function(err, results) {  
+  // We send the query to the db.query
+  db.query('truncate table tbl_users', function(err, results) { 
+    // be successful if it returns result ok 
     var response={};
     response.data=results;
     response.error=err;
@@ -15,91 +39,133 @@ exports.truncate=function(callback){
   });
 }
 
+// The function is responsible for validateLogin searching email and password
+// parameters to json Receives it with username and password in model
+// in our database if found returns a json us all
+// user information to the callback is another param.
 exports.validateLogin=function(model,callback){
+  // validate that the data is correct
   var isValid=validateModel(model);
   if(isValid.status){
+    // we send a query to find out if the user you seek
     db.select('id,email,password').where('email= "'+model.email+
-      '" and password="'+ model.password+'"').get('tbl_users',function(err,data){         
+      '" and password="'+ model.password+'"').get('tbl_users',function(err,data){
+      //if no error means that the consultation was a success
+      //login is correct       
       if(err==null){ 
         if(data.length>0){
           callback(data);
         }else{
+          // If we send wrong user credentials error.
           callback({data:"",error:"User and password are incorrect "});
         }
       }else{
+          // If there was an error in the query sent an error.
           callback({error:err});
       }
     });
   }else{
+    // If the input data is incorrect send the error.
     callback({error:isValid.error});
   } 
 }
 
+// The function findAll allows us to bring all users that exist in our table
+// Returns a callback with the response in json.
+
 exports.findAll=function(callback){
+  // We send the query to the table tbl_users
   db.select('nombre,apellido,email,password').get('tbl_users',function(err,data){
+    // If the error is null we send the user there.
     if(err==null){
       callback(data);
     }else{
+      // If there is an error send error.
       callback(err);
     } 
   });
 }
 
+// Save function saves the user receives as parameters a type json
+// {nombre: "xxx", apellido: "xxxx", email: "jj@hhh.com", password: "jfiowj123231"}
+// and responds with a callback if the answer has been saved
+
 exports.save=function(model,callback){
+  // We validate the input data
   var isValid=validateModel(model);
     if(isValid.status){
       db.select('id,nombre,apellido,email,password').where('email="'+model.email
         +'" and password="'+model.password+'"').get('tbl_users',function(err,data){
+        // If the user exists send alert
         if(data!=""&& data!=null){
           callback({data:"",error:"The username already exists"});  
         }else{
+          // If the store does not exist.
           db.insert('tbl_users', { nombre: model.nombre , apellido: model.apellido,
             email:model.email ,password:model.password }, function(err, data){
             if(err==null){
+              // Answer ok
               callback(data);
             }else{
+              // If there was an error in the query sent an error
               callback({error:err});
             }                 
           });
         }  
       });
     }else{
+    // If the input data is incorrect send the error
     callback({error:isValid.error});
     }     
 }
 
+// The findById function allows you to search a user by their unique identifier
+// Receives as parameters a type json {id: userid} and if the user is found
+// Responds with a callback with all user information
 
 exports.findById=function(model,callback){
+  // we validate that the ID is an integer 
   model.id=parseInt(model.id);
   if(isInt(model.id)){
     db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',
       function(err,data){
         if(err==null){
+          // if the response contains the data sent
           if(data!=null && data!=""){
             callback(data);
           }else{
+            // if the id is not found the user does not exist and send response
             callback({error:"User Not exist"})
           }
         }else{
+          // If there was an error in the query sent an error
           callback(err);
         }  
     });
   }else{
+    // If the user id is different from a number sent Error
     callback({error:"The userid is invalid"});
   }
 }
 
-
+// The update function allows us to update a user by sending
+// One with json data to update the json is {email: "," nombre "," param "value"}
+// If the data is correct we answer a callback with the updated information
 exports.update=function(model,callback){
+  // we validate that the ID is an integer 
   model.id=parseInt(model.id);
   if(isInt(model.id)){
+    // We verify that the user exists
     db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',
       function(err,data){
         if(data.length>0){
+          // validate input data 
           var isValid=validateModel(model);
           if(isValid.status){
             db.where({id:model.id});
+            // Send the data to be updated
             db.update('tbl_users', model , function(err){
+              // If there is no error in the query send any updated information
               if(err==null){
                 db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',
                   function(err,result){
@@ -110,49 +176,65 @@ exports.update=function(model,callback){
                     callback(response.data);
                 });
               }else{
+                // If there was an error in the query sent an error
                 callback({error:err});
               }    
             });
           }else{
+            // If the input data is incorrect send an error
             callback({error:isValid.error});
           } 
         }else{
+          // if the submitted user id does not exist, send the error
           callback({error:"User not found"});
         }   
     });
   }else{
+      // If the user id is different from a number dispatched Error
       callback({error:"The userid is invalid"});
   }
 }
 
+// Delete allows us to delete a user sending the id as a parameter in json {id: userId}
+// If user exists is removed and responds with a callback Answered removed
 exports.delete=function(model,callback){
+  // We validate that the ID is an integer 
   model.id=parseInt(model.id);
   if(isInt(model.id)){
+    // We verify that the user exists
     db.select('id,nombre,apellido,email,password').where('id='+model.id).get('tbl_users',function(err,data){
       if(data!=null && data!=""){
+        // if the user exists send query to delete
         db.where({ id: model.id }).delete('tbl_users', function(err){
           if(err==null){
            callback({data:"Deleted"});  
           }else{
+            // If the input data is incorrect send an error
             callback({error:err});
           }
         });
       }else{
+        // if the submitted user id does not exist, send the error
         callback({error:"User not found"});
       }   
     });
   }else{
+    // If the user id is different from a number dispatched Error
     callback({error:"The userid is invalid"});
   }
 }
 
+
+// isInt verifies that data is an integer
 function isInt(n) {
    return typeof n === 'number' && parseFloat(n) == parseInt(n, 10) && !isNaN(n);
 }
 
+
+// Validate empty, email, data and sql injection.
 function validateModel(data){
   var validData={status:1,error:""};
-  var expreg={email:/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,other:"^$"};
+  var expreg={email:/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,other:"^$",tags:"^[a-zA-Z0-9]+$"};
   for(i in data){
     var key = i;
     if(key==='email'){
@@ -164,8 +246,13 @@ function validateModel(data){
     if(key!='email' && key != 'id'){
       if(data[key].match(expreg.other)){
         validData.status=0;
-        validData.error+= " Error:"+ key +" is required. "
-      }      
+        validData.error+= " Error:"+ key +" is required.";
+      }
+      if(!data[key].match(expreg.tags)){
+        console.log("entro");
+        validData.status=0;
+        validData.error+="Error: "+key+" contains invalid characters.";
+      } 
     }
   }
   return validData;
--- app/routes/user.js
+++ app/routes/user.js
@@ -1,21 +1,7 @@
 // API RESTFUL USERS
 // Then send call our model to make use of its functions
-// We send one call activerecord-mysql module that allows us to have communication
-// with the database
 
 var model=require('../models/user.js');
-var Db = require('mysql-activerecord');
-
-// db is our object to access our database
-// server is the database server
-// database username and password for the database
-// so the name of the same database
-var db = new Db.Adapter({
-  server: 'localhost', 
-  username: 'root',
-  password: '',
-  database: 'intangibledb'
-});
 
 /* Truncar tabla */
 exports.db=function(req,res){
@@ -24,26 +10,26 @@ exports.db=function(req,res){
   })
 }
 
-//method: Login 
-//request POST 
-//params: username,password
+// method: Login 
+// request POST 
+// params: username,password
 // This function makes sending login and password
 exports.login=function(req,res){
-  //We receive data via http
+  // We receive data via http
   var email=req.body.email.trim();
   var password=req.body.password.trim();
   // Send the data in json format type, the model to be validated
   // and make the match.
   model.validateLogin({email:email,password:password},function(callback){
-    //In the callback variable you receive the answer in json
+    // In the callback variable you receive the answer in json
     res.send(callback);
   });    
 }
 
-//method: list 
-//request GET 
-//params: none
-//Description:
+// method: list 
+// request GET 
+// params: none
+// Description:
 // The list function brings all users that exist in the
 // database
 exports.list = function(req, res){
@@ -53,67 +39,68 @@ exports.list = function(req, res){
   });
 };
 
-//method: create 
-//request POST /users/add
-//params: nombre,apellido,email,password
+// method: create 
+// request POST /users/add
+// params: nombre,apellido,email,password
 // The create function lets you create a new user
-//get name, last name, email, password.
+// get name, last name, email, password.
 exports.create=function(req,res){
   model.save(req.body, function(callback){  
     //We send the response to the client.
     res.send(callback);
   });
 }
-/*
-   method: view
-   request GET /users/:id 
-   params: id (user id PK)
-   The view function allows us to bring information from a user sending
-   the id through a GET request
-*/
+
+// method: view
+// request GET /users/:id 
+// params: id (user id PK)
+// The view function allows us to bring information from a user sending
+// the id through a GET request
+
 exports.view=function(req,res){
   // Take the id of the url sent by GET  var id=req.params.id; 
-  //  validate that is an integer
+  // validate that is an integer
+  var id=req.params.id;
   if(id.match(/^\d+/)){
     model.findById({id:id},function(callback){
-      //We send the response to the client.
+      // We send the response to the client.
       res.send(callback);  
     });
   }else{
-    //if you receive a different parameter to an integer
-    //send an error code
+    // if you receive a different parameter to an integer
+    // send an error code
     res.status(430).send({error:"Invalid Request Error! "});
   } 
 }
-/*
-   method: update 
-   request PUT /users/edit/:id
-   params:id user id
-   La funcion update nos permite actualizar un usuario enviando como parametros
-   los datos a actualizar es necesario el id de usuario. 
-*/
+
+// method: update 
+// request PUT /users/edit/:id
+// params:id user id
+// La funcion update nos permite actualizar un usuario enviando como parametros
+// los datos a actualizar es necesario el id de usuario. 
+
 exports.update=function(req,res){
   // Take the id of the url sent by GET  var id=req.params.id; 
-  //  validate that is an integer
+  // validate that is an integer
   var id=req.params.id;
   if(id.match(/^\d+/)){
     req.body.id=id;
     model.update(req.body, function(callback){
-      //We send the response to the client.
+      // We send the response to the client.
       res.send(callback);           
     });
   }else{
-    //if you receive a different parameter to an integer
-    //send an error code
+    // if you receive a different parameter to an integer
+    // send an error code
     res.status(430).send({error:"Invalid Request Error! "});
   } 
 }
-/*
-   method: delete
-   request DELETE /users/:id
-   params:id user id 
-   This function deletes a user by sending the user id for GET
-*/
+
+// method: delete
+// request DELETE /users/:id
+// params:id user id 
+// This function deletes a user by sending the user id for GET
+
 exports.delete=function(req,res){ 
   // Take the id of the url sent by GET  var id=req.params.id; 
   //  validate that is an integer
@@ -121,7 +108,7 @@ exports.delete=function(req,res){
   if(id.match(/^\d+/)){
     // send the id of the user to remove the model
     model.delete({id:id},function(callback){
-      //We send the response to the client.
+      // We send the response to the client.
       res.send(callback);
     });
   }
